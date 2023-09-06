0d45e27e40ddfa2b9ce9d02018a240655b7a5c4e
AznRaven <74807576+AznRaven@users.noreply.github.com>
2023-09-06 18:41:57 -0500

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [data, setData] = useState([]);
+  const [random, setRandom] = useState(0);
+  const limit = 3;
+  const apiKey = process.env.REACT_APP_KEY;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://api.api-ninjas.com/v1/jokes?limit=${limit}`, {
+          headers: { "X-Api-Key": apiKey },
+          responseType: "json",
+        });
+        setData(response.data);
+      } catch (error) {
+        if (error.response) {
+          console.error("Error: ", error.response.data);
+        } else {
+          console.error("Error: ", error.message);
+        }
+      }
+    };
+
+    fetchData();
+  }, [apiKey, limit, random]);
+
+  console.log(data);
+
+  return (
+    <>
+      <h1>{data[0]?.joke}</h1>
+      <br />
+      <button onClick={() => setRandom(random + 1)}>Random</button>
+    </>
+  );
+}
